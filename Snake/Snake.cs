6b1f828a2fa7d6811e47232a89a2af77d9995f87
Dafiruz <m4gn3t46@gmail.com>
2017-08-12 01:58:14 +0100

    added some code to the snake class

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -9,6 +9,33 @@ namespace Snake
 {
     public class Snake
     {
+        public Rectangle[] snake;
+        private SolidBrush brush;
+        private int x, y, width, height;
 
+        public Snake()
+        {
+            snake = new Rectangle[3];
+            brush = new SolidBrush(Color.Gray);
+
+            x = 20;
+            y = 0;
+            width = 10;
+            height = 10;
+
+            for (int i = 0; i < snake.Length; i++)
+            {
+                snake[i] = new Rectangle(x, y, width, height);
+                x -= 10;
+            }
+        }
+
+        public void draw(Graphics paper)
+        {
+            foreach(Rectangle rec in snake)
+            {
+                paper.FillRectangle(brush, rec);
+            }
+        }
     }
 }
